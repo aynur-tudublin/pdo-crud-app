ee3bc60df826d4693fd7e5acdafab253c8eae556
Aynur Hasanova <b00177512@mytudublin.ie>
2025-11-11 20:39:15 +0000

    Add user action links to index.php

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php include "templates/header.php"; ?>
+
+<section class="stack-lg">
+  <div class="card">
+    <h2 class="card__title">Actions</h2>
+    <ul class="list">
+      <li><a class="btn" href="create.php">Create</a> <span class="muted"> - add a user</span></li>
+      <li><a class="btn btn--ghost" href="read.php">Read</a> <span class="muted"> - find a user</span></li>
+      <li><a class="btn btn--ghost" href="update.php">Read</a> <span class="muted"> - edit a user</span></li>
+    </ul>
+  </div>
+
+  <div class="note">
+    <strong>Tip:</strong> We can use “Create” to insert a few test users, then “Read” to filter by <em>Location</em>.
+  </div>
+</section>
+
+<?php include "templates/footer.php"; ?>
