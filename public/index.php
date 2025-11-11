cccff9ec1c688289463d06483319558e88baab67
Aynur Hasanova <b00177512@mytudublin.ie>
2025-11-11 21:12:39 +0000

    Add delete user option to index.php

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@
       <li><a class="btn" href="create.php">Create</a> <span class="muted"> - add a user</span></li>
       <li><a class="btn btn--ghost" href="read.php">Read</a> <span class="muted"> - find a user</span></li>
       <li><a class="btn btn--ghost" href="update.php">Update</a> <span class="muted"> - edit a user</span></li>
+      <li><a class="btn btn--ghost" href="delete.php">Delete</a> <span class="muted"> - delete a user</span></li>
     </ul>
   </div>
 
